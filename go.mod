ba951faac1a14efc85eee2bebc4d10d7844bde43
Eyob <eabebe91@gmail.com>
2026-01-01 21:44:33 +0300

    Refactor OAuth2 implementation: remove config package and handlers, integrate logic directly in main.go, and load environment variables using godotenv

--- go.mod
+++ go.mod
@@ -5,6 +5,11 @@ go 1.25.4
 tool github.com/air-verse/air
 
 require (
+	github.com/joho/godotenv v1.5.1
+	golang.org/x/oauth2 v0.34.0
+)
+
+require (
 	cloud.google.com/go/compute/metadata v0.8.0 // indirect
 	dario.cat/mergo v1.0.2 // indirect
 	github.com/air-verse/air v1.63.4 // indirect
@@ -14,7 +19,6 @@ require (
 	github.com/fsnotify/fsnotify v1.9.0 // indirect
 	github.com/gobwas/glob v0.2.3 // indirect
 	github.com/gohugoio/hugo v0.149.1 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/pelletier/go-toml v1.9.5 // indirect
@@ -22,7 +26,6 @@ require (
 	github.com/spf13/afero v1.14.0 // indirect
 	github.com/spf13/cast v1.9.2 // indirect
 	github.com/tdewolff/parse/v2 v2.8.3 // indirect
-	golang.org/x/oauth2 v0.34.0 // indirect
 	golang.org/x/sys v0.35.0 // indirect
 	golang.org/x/text v0.28.0 // indirect
 	google.golang.org/protobuf v1.36.8 // indirect
--- main.go
+++ main.go
@@ -1,14 +1,55 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net"
 	"net/http"
+	"os"
 
-	"github.com/eyoba-bisru/goauth/handlers"
 	"github.com/joho/godotenv"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
 )
+
+var OauthConfig *oauth2.Config
+
+func LoginHandler(w http.ResponseWriter, r *http.Request) {
+	url := OauthConfig.AuthCodeURL("random-state", oauth2.AccessTypeOffline)
+	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
+}
+
+func CallbackHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := context.Background()
+
+	code := r.URL.Query().Get("code")
+	if code == "" {
+		http.Error(w, "Code not found", http.StatusBadRequest)
+		return
+	}
+
+	token, err := OauthConfig.Exchange(ctx, code)
+	if err != nil {
+		http.Error(w, "Token exchange failed: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Println("token:", token)
+
+	client := OauthConfig.Client(ctx, token)
+
+	resp, err := client.Get("https://www.googleapis.com/oauth2/v2/userinfo")
+	if err != nil {
+		http.Error(w, "Failed to get user info", http.StatusInternalServerError)
+		return
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("resp:", resp)
+
+	w.Write([]byte("Login successful"))
+}
 
 func main() {
 
@@ -17,8 +58,19 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
-	http.HandleFunc("/login", handlers.LoginHandler)
-	http.HandleFunc("/callback", handlers.CallbackHandler)
+	OauthConfig = &oauth2.Config{
+		ClientID:     os.Getenv("GOOGLE_CLIENT_ID"),
+		ClientSecret: os.Getenv("GOOGLE_CLIENT_SECRET"),
+		RedirectURL:  "http://localhost:8080/callback",
+		Scopes: []string{
+			"https://www.googleapis.com/auth/userinfo.email",
+			"https://www.googleapis.com/auth/userinfo.profile",
+		},
+		Endpoint: google.Endpoint,
+	}
+
+	http.HandleFunc("/login", LoginHandler)
+	http.HandleFunc("/callback", CallbackHandler)
 
 	ln, err := net.Listen("tcp", ":8080")
 	if err != nil {
