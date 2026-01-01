685b81d2e932d0c192ce127f1436cb559c030aed
Eyob <eabebe91@gmail.com>
2026-01-01 21:16:25 +0300

    Add initial project setup with OAuth2 login functionality
    - Create configuration for OAuth2 in config/config.go
    - Implement login and callback handlers in handlers/handlers.go
    - Set up .env.example and .gitignore files
    - Update go.mod and go.sum with necessary dependencies
    - Initialize main.go to handle login and callback routes
    - Add .air.toml for development environment configuration

--- main.go
+++ main.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+
+	"github.com/eyoba-bisru/goauth/handlers"
+	"github.com/joho/godotenv"
+)
+
 func main() {
 
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	http.HandleFunc("/login", handlers.LoginHandler)
+	http.HandleFunc("/callback", handlers.CallbackHandler)
+
+	ln, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatal(err) // port not available, permission denied, etc.
+	}
+
+	fmt.Println("Server is running on port 8080")
+
+	log.Fatal(http.Serve(ln, nil))
 }
